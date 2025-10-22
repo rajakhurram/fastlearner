7dce3123e0d04fc85398ec5d3fc489f524fab363
Qasim Ali <qasim@vinncorp.com>
2025-10-22 17:05:00 +0500

    Updated the environment.ts file

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,16 @@
-
 export const environment = {
   production: true,
-  isProductionServer: false,
+  isProductionServer: true,
+
+  basePath: `http://localhost`,
+  loginUrl: `http://localhost:8443/auth`,
+  baseUrl: `http://localhost:8443/api/v1/`,
+  videoUrl: `http://localhost:8443`,
+  imageUrl: `http://localhost:8443`,
+  applicationCourseContentUrl: `http://localhost/student/course-content/`,
+  applicationCourseDetailsUrl: `http://localhost/student/course-details/`,
+
+  // Goolge Login Credentials
+  googleClientId: '{{google-client-id}}',
+  googleSecretKey: '{{google-secret-key}}',
 };
