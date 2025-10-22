94930c9d0bdd17bf3c78368c4f353c54ed1d7875
Qasim Ali <qasim@vinncorp.com>
2025-10-22 17:24:37 +0500

    Updated the domain form localhost to host IP

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
   isProductionServer: true,
 
-  basePath: `http://localhost`,
-  loginUrl: `http://localhost:8443/auth`,
-  baseUrl: `http://localhost:8443/api/v1/`,
-  videoUrl: `http://localhost:8443`,
-  imageUrl: `http://localhost:8443`,
-  applicationCourseContentUrl: `http://localhost/student/course-content/`,
-  applicationCourseDetailsUrl: `http://localhost/student/course-details/`,
+  basePath: `http://34.57.196.83`,
+  loginUrl: `http://34.57.196.83:8443/auth`,
+  baseUrl: `http://34.57.196.83:8443/api/v1/`,
+  videoUrl: `http://34.57.196.83:8443`,
+  imageUrl: `http://34.57.196.83:8443`,
+  applicationCourseContentUrl: `http://34.57.196.83/student/course-content/`,
+  applicationCourseDetailsUrl: `http://34.57.196.83/student/course-details/`,
 
   // Goolge Login Credentials
   googleClientId: '{{google-client-id}}',
