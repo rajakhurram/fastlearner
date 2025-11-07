7f211c3e1ecd348bb2c3e3f7c6217ebe7cb8248a
Abdul Wassay <abdulwassy@vinncorp.com>
2025-11-07 12:33:46 +0500

    env

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
   isProductionServer: true,
 
-  basePath: `http://35.193.81.222`,
-  loginUrl: `http://35.193.81.222:8443/auth`,
-  baseUrl: `http://35.193.81.222:8443/api/v1/`,
-  videoUrl: `http://35.193.81.222:8443`,
-  imageUrl: `http://35.193.81.222:8443`,
-  applicationCourseContentUrl: `http://35.193.81.222/student/course-content/`,
-  applicationCourseDetailsUrl: `http://35.193.81.222/student/course-details/`,
+  basePath: `http://34.46.43.246`,
+  loginUrl: `http://34.46.43.246:8443/auth`,
+  baseUrl: `http://34.46.43.246:8443/api/v1/`,
+  videoUrl: `http://34.46.43.246:8443`,
+  imageUrl: `http://34.46.43.246:8443`,
+  applicationCourseContentUrl: `http://34.46.43.246/student/course-content/`,
+  applicationCourseDetailsUrl: `http://34.46.43.246/student/course-details/`,
 
   // Goolge Login Credentials
   googleClientId: '{{google-client-id}}',
