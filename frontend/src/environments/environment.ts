1114e7dae4ace348f0f81b6d59c96d6e59c0b740
qaximbalti <123444698+qaximbalti@users.noreply.github.com>
2026-01-19 18:56:44 +0500

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
   isProductionServer: true,
 
-  basePath: `http://qualitycenter.fastlearner.ai`,
-  loginUrl: `http://qualitycenter.fastlearner.ai/auth`,
-  baseUrl: `http://qualitycenter.fastlearner.ai/api/v1/`,
-  videoUrl: `http://qualitycenter.fastlearner.ai`,
-  imageUrl: `http://qualitycenter.fastlearner.ai`,
-  applicationCourseContentUrl: `http://qualitycenter.fastlearner.ai/student/course-content/`,
-  applicationCourseDetailsUrl: `http://qualitycenter.fastlearner.ai/student/course-details/`,
+  basePath: `http://136.119.202.42`,
+  loginUrl: `http://136.119.202.42:8443/auth`,
+  baseUrl: `http://136.119.202.42:8443/api/v1/`,
+  videoUrl: `http://136.119.202.42:8443`,
+  imageUrl: `http://136.119.202.42:8443`,
+  applicationCourseContentUrl: `http://136.119.202.42:8443/student/course-content/`,
+  applicationCourseDetailsUrl: `http://136.119.202.42:8443/student/course-details/`,
 
   // Goolge Login Credentials
   googleClientId: '{{google-client-id}}',
