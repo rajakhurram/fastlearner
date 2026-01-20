f977d01ec552728f738ffc744e0c9721a7b91c3f
qaximbalti <123444698+qaximbalti@users.noreply.github.com>
2026-01-21 02:48:26 +0500

    Update 38 environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
   isProductionServer: true,
 
-  basePath: `http://136.119.202.42`,
-  loginUrl: `http://136.119.202.42:8443/auth`,
-  baseUrl: `http://136.119.202.42:8443/api/v1/`,
-  videoUrl: `http://136.119.202.42:8443`,
-  imageUrl: `http://136.119.202.42:8443`,
-  applicationCourseContentUrl: `http://136.119.202.42:8443/student/course-content/`,
-  applicationCourseDetailsUrl: `http://136.119.202.42:8443/student/course-details/`,
+  basePath: `http://34.9.170.10`,
+  loginUrl: `http://34.9.170.10:8443/auth`,
+  baseUrl: `http://34.9.170.10:8443/api/v1/`,
+  videoUrl: `http://34.9.170.10:8443`,
+  imageUrl: `http://34.9.170.10:8443`,
+  applicationCourseContentUrl: `http://34.9.170.10:8443/student/course-content/`,
+  applicationCourseDetailsUrl: `http://34.9.170.10:8443/student/course-details/`,
 
   // Goolge Login Credentials
   googleClientId: '{{google-client-id}}',
