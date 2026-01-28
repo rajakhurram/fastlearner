71fe91e09fea64ee3238e86f0b1ad32076897b44
qaximbalti <123444698+qaximbalti@users.noreply.github.com>
2026-01-28 13:07:02 +0500

    Update lassroom.aqualitycenter.com environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
   isProductionServer: true,
 
-  basePath: `http://34.9.170.10`,
-  loginUrl: `http://34.9.170.10:8443/auth`,
-  baseUrl: `http://34.9.170.10:8443/api/v1/`,
-  videoUrl: `http://34.9.170.10:8443`,
-  imageUrl: `http://34.9.170.10:8443`,
-  applicationCourseContentUrl: `http://34.9.170.10:8443/student/course-content/`,
-  applicationCourseDetailsUrl: `http://34.9.170.10:8443/student/course-details/`,
+  basePath: `https://classroom.aqualitycenter.com`,
+  loginUrl: `https://classroom.aqualitycenter.com:8443/auth`,
+  baseUrl: `https://classroom.aqualitycenter.com:8443/api/v1/`,
+  videoUrl: `https://classroom.aqualitycenter.com:8443`,
+  imageUrl: `https://classroom.aqualitycenter.com:8443`,
+  applicationCourseContentUrl: `https://classroom.aqualitycenter.com:8443/student/course-content/`,
+  applicationCourseDetailsUrl: `https://classroom.aqualitycenter.com:8443/student/course-details/`,
 
   // Goolge Login Credentials
   googleClientId: '{{google-client-id}}',
